b8ec469d029639f2ed1efb46ad88aff2a954350f
anitaSF <ana.isabel.santos@bootcamp-upgrade.com>
2026-08-27 19:13:43 +0200

    credenciales login cargadas

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,9 +5,10 @@ import { useNavigate } from "react-router-dom";
 import logo from '../../images/logotipo-login.webp';
 
 function Login({ setUserData }) {
+    // Credenciales demo cargadas por defecto
     const INITIAL_STATE = {
-        username: '',
-        password: '',
+        username: 'emilys',
+        password: 'emilyspass',
     };
 
     const navigate = useNavigate();
@@ -38,15 +39,18 @@ function Login({ setUserData }) {
             <div className="card-login">
                 <img className="logo-img" src={logo} />
                 <h4 className="txt-login">Crea tus propios menus y empieza la semana comiendo sano y rico</h4>
-                <form action='' onChange={handleInput} onSubmit={handleForm}>
+                <p style={{ fontSize: '0.8rem', opacity: 0.8, marginBottom: '1rem', textAlign: 'center' }}>
+                    💡 <strong>Acceso Demo:</strong> Credenciales precargadas. Haz clic en "Login".
+                </p>
+                <form action='' onSubmit={handleForm}>
 
                     <div>
                         <label htmlFor='username'>Nombre de usuario</label>
-                        <input type="text" id='username' name='username' />
+                        <input type="text" id='username' name='username' value={user.username} onChange={handleInput} />
                     </div>
                     <div>
                         <label htmlFor='password'>Contraseña</label>
-                        <input type='password' name='password' id='password' />
+                        <input type='password' name='password' id='password' value={user.password} onChange={handleInput} />
                     </div>
                     <button className="btn">Login</button>
 
